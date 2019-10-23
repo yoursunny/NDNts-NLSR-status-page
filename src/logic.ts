f1a17d2dd8538d782c5386a2ef889323467f1177
Junxiao Shi <git@mail1.yoursunny.com>
2019-10-23 19:26:15 -0400

    Use discoverVersion from @ndn/segmented-object

--- src/logic.ts
+++ src/logic.ts
@@ -1,9 +1,8 @@
-import { Forwarder, SimpleEndpoint } from "@ndn/fw";
+import { Forwarder } from "@ndn/fw";
 import { L3Face } from "@ndn/l3face";
-import { Interest } from "@ndn/l3pkt";
 import { Name } from "@ndn/name";
-import { Segment as Segment02 } from "@ndn/naming-convention-02";
-import { fetch } from "@ndn/segmented-object";
+import { Segment as Segment02, Version as Version02 } from "@ndn/naming-convention-02";
+import { discoverVersion, fetch } from "@ndn/segmented-object";
 import { Decoder } from "@ndn/tlv";
 import { WsTransport } from "@ndn/ws-transport";
 
@@ -16,15 +15,16 @@ export async function connect(): Promise<void> {
 }
 
 export async function fetchNameLsas(): Promise<NameLsa[]> {
-  const se = new SimpleEndpoint();
   // find version number
-  const data = await se.consume(new Interest("/ndn/edu/arizona/%C1.Router/hobo/nlsr/lsdb/names",
-                                             Interest.CanBePrefix, Interest.MustBeFresh));
+  const versioned = await discoverVersion(
+    new Name("/ndn/edu/arizona/%C1.Router/hobo/nlsr/lsdb/names"),
+    {
+      segmentNumConvention: Segment02,
+      versionConvention: Version02,
+    });
 
   // retrieve segmented object with version number
-  // TODO integrate version discovery into @ndn/segmented-object package
-  const dataset = await fetch(data.name.getPrefix(-1),
-                              { segmentNumConvention: Segment02 }).promise;
+  const dataset = await fetch(versioned, { segmentNumConvention: Segment02 }).promise;
 
   const decoder = new Decoder(dataset);
   const list = [] as NameLsa[];
