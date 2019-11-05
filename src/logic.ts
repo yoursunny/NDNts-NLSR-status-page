40a0daceade447f6bcc6c1775722ba37e276fd71
Junxiao Shi <git@mail1.yoursunny.com>
2019-11-04 22:23:13 -0500

    Use @ndn/naming-convention1 instead of @ndn/naming-convention-02

--- src/logic.ts
+++ src/logic.ts
@@ -1,7 +1,7 @@
 import { connectToTestbed } from "@ndn/autoconfig";
 import { FwTracer } from "@ndn/fw";
 import { Name } from "@ndn/name";
-import { Segment as Segment02, Version as Version02 } from "@ndn/naming-convention-02";
+import { Segment, Version } from "@ndn/naming-convention1";
 import { discoverVersion, fetch } from "@ndn/segmented-object";
 import { Decoder } from "@ndn/tlv";
 
@@ -24,10 +24,10 @@ export async function connect(): Promise<string> {
 export async function fetchNameLsas(): Promise<NameLsa[]> {
   const name = new Name("/ndn/edu/arizona/%C1.Router/hobo/nlsr/lsdb/names");
   const dataset = await discoverVersion(name, {
-    segmentNumConvention: Segment02,
-    versionConvention: Version02,
+    segmentNumConvention: Segment,
+    versionConvention: Version,
   }).then((versioned) => fetch(versioned, {
-    segmentNumConvention: Segment02,
+    segmentNumConvention: Segment,
   }).promise);
 
   const decoder = new Decoder(dataset);
