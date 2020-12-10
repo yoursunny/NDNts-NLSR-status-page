ec8a01ceaf9c85d1fb8b828222406881878ad1df
Junxiao Shi <git@mail1.yoursunny.com>
2020-12-10 10:01:31 -0500

    accept testbed root certificate x3

--- src/model/trust.ts
+++ src/model/trust.ts
@@ -3,7 +3,7 @@ import { Data, Verifier } from "@ndn/packet";
 import { Decoder, fromHex } from "@ndn/tlv";
 import { TrustSchema, TrustSchemaVerifier, versec2019 } from "@ndn/trust-schema";
 
-// curl -sL https://named-data.net/ndnsec/ndn-testbed-root-v2.ndncert | xxd -p
+// curl -sfL https://named-data.net/ndnsec/ndn-testbed-root-v2.ndncert | xxd -p
 const NDN_TESTBED_ROOT_V2_HEX = `
 06fd023b072408036e646e08034b45590808659d7fa5c581107d08036e64
 6e0809fd00000160714a519b140918010219040036ee8015fd014f308201
@@ -26,7 +26,20 @@ b736ce03e17ac622c1ee2601cd2a56a59cdf9ad5aab179639bf4a45eee2b
 3d02204c5322df3666fc487e5f4b73adc3b10e16e6b25d9c864223ca8a24
 68cf69ba43`;
 
-const NDN_TESTBED_ROOT_V2_DATA = new Decoder(fromHex(NDN_TESTBED_ROOT_V2_HEX.replace(/\s+/g, ""))).decode(Data);
+// curl -sfL https://named-data.net/ndnsec/ndn-testbed-root-x3.ndncert | xxd -p
+const NDN_TESTBED_ROOT_X3_HEX = `
+06fd0149072408036e646e08034b45590808ecf14c8e512315e008036e64
+6e0809fd00000175e67f3210140918010219040036ee80155b3059301306
+072a8648ce3d020106082a8648ce3d030107034200041b1fb763816f628d
+5e49c22f803ef8950850773378cead36d5da29f2dee419f1297c674a7f61
+c6a679f8feaa05f62275c94644ffb35af33ccee314fa46920cd116701b01
+031c16071408036e646e08034b45590808ecf14c8e512315e0fd00fd26fd
+00fe0f323032303131323054313633313337fd00ff0f3230323431323331
+54323335393539fd010227fd020023fd02010866756c6c6e616d65fd0202
+134e444e205465737462656420526f6f7420583317473045022100fc86bb
+53ea862f4d722da5fcb834883702b078b7675086029c85e6b3a8280ada02
+2053c0b1766141760e6596ba1e40712d9cf6b6a8770ea88b919a18e250d8
+09c5a9`;
 
 const POLICY = `
 sitename = <_s1> | (<_s1>/<_s2>) | (<_s1>/<_s2>/<_s3>)
@@ -41,11 +54,20 @@ lsdbdata = ndn/<sitename>/%C1.Router/<_routerid>/nlsr/lsdb/<_lsatype>/<_version>
 lsdbdata <= routercert <= operatorcert <= sitecert <= rootcert
 `;
 
+async function importRootCert(hex: string): Promise<Certificate> {
+  const data = new Decoder(fromHex(hex.replace(/\s+/g, ""))).decode(Data);
+  const cert = Certificate.fromData(data);
+  await cert.createVerifier();
+  return cert;
+}
+
 async function makeVerifier(): Promise<Verifier> {
-  const rootCert = Certificate.fromData(NDN_TESTBED_ROOT_V2_DATA);
-  await rootCert.createVerifier();
+  const [root2, root3] = await Promise.all([
+    importRootCert(NDN_TESTBED_ROOT_V2_HEX),
+    importRootCert(NDN_TESTBED_ROOT_X3_HEX),
+  ]);
   const policy = versec2019.load(POLICY);
-  const schema = new TrustSchema(policy, [rootCert]);
+  const schema = new TrustSchema(policy, [root2, root3]);
   return new TrustSchemaVerifier({ schema });
 }
 
