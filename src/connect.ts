4201696299acf280a650d7d1a88db49105808231
Junxiao Shi <git@mail1.yoursunny.com>
2021-05-28 19:04:50 -0400

    connect: connectToTestbed => connectToNetwork

--- src/connect.ts
+++ src/connect.ts
@@ -1,48 +1,17 @@
-import { connectToTestbed } from "@ndn/autoconfig";
-import { Endpoint } from "@ndn/endpoint";
-import type { FwFace } from "@ndn/fw";
-import { Name } from "@ndn/packet";
+import { connectToNetwork } from "@ndn/autoconfig";
 import { H3Transport } from "@ndn/quic-transport";
-import pAny from "p-any";
-
-async function connectH3(): Promise<string> {
-  const abort = new AbortController();
-  let face: FwFace|undefined;
-  try {
-    face = await H3Transport.createFace({
-      lp: { mtu: 1200 },
-    }, "https://lil.quic.g.ndn.today:6367/ndn");
-    face.addRoute(new Name("/"));
-
-    const endpoint = new Endpoint({ signal: abort.signal });
-    const suffix = Math.floor(Math.random() * 1e8);
-    await pAny([
-      endpoint.consume(`/ndn/edu/arizona/ping/${suffix}`),
-      endpoint.consume(`/ndn/nl/delft/ping/${suffix}`),
-      endpoint.consume(`/ndn/my/edu/uum/ping/${suffix}`),
-    ]);
-
-    return face.toString();
-  } catch (err: unknown) {
-    console.warn("HTTP/3 connection error", err);
-    face?.close();
-    throw err;
-  } finally {
-    abort.abort();
-  }
-}
 
 export async function connect(): Promise<string> {
-  try { return await connectH3(); } catch {}
-
-  const faces = await connectToTestbed({
-    connectTimeout: 5000,
-    count: 4,
-    fchFallback: ["hobo.cs.arizona.edu", "ndn-testbed.ewi.tudelft.nl", "uum.testbed.named-data.net"],
-    preferFastest: true,
+  const faces = await connectToNetwork({
+    H3Transport,
+    fallback: ["hobo.cs.arizona.edu", "ndn-testbed.ewi.tudelft.nl", "uum.testbed.named-data.net"],
+    testConnection: [
+      "/ndn/edu/arizona/ping/*",
+      "/ndn/nl/delft/ping/*",
+      "/ndn/my/edu/uum/ping/*",
+      "/yoursunny/_/dal/ping/*",
+      "/yoursunny/_/waw/ping/*",
+    ],
   });
-  if (faces.length === 0) {
-    throw new Error("unable to connect to NDN testbed");
-  }
-  return faces[0].toString();
+  return faces[0]!.toString();
 }
