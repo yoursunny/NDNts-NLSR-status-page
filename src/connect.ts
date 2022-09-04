674abaa3081bcb6392046e22fe07b926c939ecc4
Junxiao Shi <git@mail1.yoursunny.com>
2022-09-04 15:09:56 +0000

    upgrade to TypeScript 4.8 and target ES2022

--- src/connect.ts
+++ src/connect.ts
@@ -2,11 +2,6 @@ import { connectToNetwork, connectToRouter } from "@ndn/autoconfig";
 import { H3Transport } from "@ndn/quic-transport";
 
 export async function connect(): Promise<string> {
-  // PCNL has no peering with NDN testbed, make it go to yoursunny ndn6 network
-  void connectToRouter("wss://nrt.ws.ndn.net.eu.org/ws/", {
-    addRoutes: ["/pcnl"],
-  });
-
   const faces = await connectToNetwork({
     H3Transport,
     fallback: ["hobo.cs.arizona.edu", "ndn.qub.ac.uk", "ndntestbed.iiit.ac.in"],
@@ -19,5 +14,14 @@ export async function connect(): Promise<string> {
       "/yoursunny/_/sin/ping/*",
     ],
   });
-  return faces[0]!.toString();
+  const uri = `${faces[0]}`;
+
+  if (!uri.includes(".ndn.net.eu.org")) {
+    // PCNL has no peering with NDN testbed, make it go to yoursunny ndn6 network
+    void connectToRouter("wss://sea.ws.ndn.net.eu.org/ws/", {
+      addRoutes: ["/pcnl"],
+    });
+  }
+
+  return uri;
 }
